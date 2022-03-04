80cdfd0fd675aecbfe76d2a7600c462f24b7f1ca
Jonathan Jopp <jonathanjopp@gmail.com>
2022-03-04 23:47:51 +0100

    Added really basic plotting proof of concept

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use bevy::{
 };
 use bevy_pixels::prelude::*;
 
+// TODO: These variables have to react to change of window size!
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
@@ -30,6 +31,26 @@ fn main() {
         .add_system(resize_notificator)
         .add_system(draw)
         .run();
+}
+
+struct FunctionValueProvider<T>
+where T: Fn(i64) -> i64
+{
+    function: T,
+}
+
+impl<T> FunctionValueProvider<T>
+where T: Fn(i64) -> i64
+{
+    fn new(function: T) -> FunctionValueProvider<T> {
+        Self {
+            function
+        }
+    }
+
+    fn get(&self, x: i64) -> i64 {
+        HEIGHT as i64 - (self.function)(x)
+    }
 }
 
 struct Color {
@@ -63,6 +84,9 @@ fn resize_notificator(
         pixels_ressource
             .pixels
             .resize_buffer(e.width as u32, e.height as u32);
+        pixels_ressource
+            .pixels
+            .resize_surface(e.width as u32, e.height as u32);
     }
 }
 
@@ -72,4 +96,15 @@ fn draw(mut pixels_ressource: ResMut<PixelsResource>) {
         .to_u8_slice()
         .repeat(frame.len() / 4);
     frame.copy_from_slice(buffer);
+    draw_pixel(0, 0, Color::rgb(255, 0, 0), frame);
+    let f = FunctionValueProvider::new(|x| x*x);
+    for i in 0..WIDTH - 1 {
+        draw_pixel(i, f.get(i.into()).try_into().unwrap_or(HEIGHT), Color::rgb(255, 0, 0), frame);
+    }
+}
+
+fn draw_pixel(x: u32, y: u32, color: Color, frame: &mut [u8]) -> &[u8] {
+    let i: usize = (4 * (x + y * HEIGHT)).try_into().unwrap();
+    frame[i..i + 4].copy_from_slice(&Color::rgb(255, 0, 0).to_u8_slice());
+    frame
 }
